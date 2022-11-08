a4cf7bd5c5696d69499b61ec30e6b3f00ef50813
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-11-08 20:17:05 -0300

    added form page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
+const messages = [
+	{
+		text: "Hi there!",
+		user: "Amando",
+		added: new Date()
+	},
+	{
+		text: "Hello World!",
+		user: "Charles",
+		added: new Date()
+	}
+];
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { messages, title: 'Mini Messageboard' });
+});
+
+router.post('/index', (req, res, next) => {
+	const { text, user } = req.body;
+	messages.push({ text, user, added: new Date() });
+	res.redirect('/');
+});
+
+router.post('/new', (req, res, next) => {
+	res.redirect('/new');
 });
 
 module.exports = router;
